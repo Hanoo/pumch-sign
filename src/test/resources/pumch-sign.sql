49934cdbbde2bed439f55b89f354110e81ba5783
Hanoo <yanhan09@163.com>
2019-02-04 16:05:34 +0800

    补充创建时间字段到“课程”表中。

--- src/test/resources/pumch-sign.sql
+++ src/test/resources/pumch-sign.sql
@@ -92,6 +92,7 @@ CREATE TABLE course (
   course_name   VARCHAR(250) NOT NULL COMMENT '课程名称',
   course_type   CHAR(1) COMMENT '课程类型，选修或者必修',
   t_id          BIGINT(20) COMMENT '任课教师ID',
+  create_time   DATETIME   COMMENT '课程创建时间',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='课程表';
 
